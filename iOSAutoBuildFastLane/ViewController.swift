979d2f25daa45e2b37b8a6abb220e0570ce821a6
Bowen <pwhsueh@gmail.com>
2016-12-15 11:56:55 +0800

    ffffff

--- iOSAutoBuildFastLane/ViewController.swift
+++ iOSAutoBuildFastLane/ViewController.swift
@@ -9,15 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.label.text = getLangCode()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    
+    func getLangCode() -> String
+    {
+        //        var language = String()
+        //bowen
+        var language = "en-us"
+        let languages = NSLocale.preferredLanguages[0]
+        let locale: NSLocale = NSLocale(localeIdentifier: languages)
+        
+        language = locale.object(forKey: NSLocale.Key.identifier) as! String
+        
+        if language.lowercased() == "zh-hant-tw" || language.lowercased() == "zh" || language.lowercased() == "zh-hant-us" || language.lowercased() == "zh-us" {
+            language = "zh-tw"
+        }
+        //        if language.lowercaseString != "zh-tw" && language.lowercaseString != "zh-cn" && language.lowercaseString != "en-us" {
+        //
+        //            if language.lowercaseString == "zh" || language.lowercaseString == "zh-hant-us" {
+        //                language = "zh-tw"
+        //            }
+        //        }
+        print("language : \(language)")
+        
+        
+        //        let currentLanguage = NSUserDefaults.standardUserDefaults().objectForKey("AppleLanguages")?.objectAtIndex(0)
+        //        if ((currentLanguage?.lowercaseString.rangeOfString("zh-tw")) != nil)
+        //        {
+        //            language = "zh-tw"
+        //        }
+        //        else if ((currentLanguage?.lowercaseString.rangeOfString("zh-hk")) != nil)
+        //        {
+        //            language = "zh-tw"
+        //        }
+        //        else if ((currentLanguage?.lowercaseString.rangeOfString("en")) != nil)
+        //        {
+        //            language = "en-us"
+        //        }
+        //        else
+        //        {
+        //            language = "en-us"
+        //        }
+        
+        
+        return language
     }
 
 
